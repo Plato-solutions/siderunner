21bb0726fb38ab4671d966e3fc0a78ed820c1abe
Maxim Zhiburt <zhiburt@gmail.com>
2020-08-02 20:50:43 +0300

    parser: add basic test
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -3,11 +3,11 @@
 // TODO: do we need Target tag?
 // TODO: create a default errors?
 
-use crate::{error::ParseError, Result};
-use std::result;
+use crate::error::ParseError;
+use std::result::Result;
 use std::time::Duration;
 
-pub fn parse<R: std::io::Read>(side_file: R) -> Result<Vec<Test>> {
+pub fn parse<R: std::io::Read>(side_file: R) -> Result<Vec<Test>, ParseError> {
     let side: format::SideFile =
         serde_json::from_reader(side_file).map_err(|err| ParseError::FormatError(err))?;
     let mut tests = Vec::new();
@@ -27,7 +27,7 @@ pub fn parse<R: std::io::Read>(side_file: R) -> Result<Vec<Test>> {
     Ok(tests)
 }
 
-fn parse_cmd(command: &format::Command) -> Result<Command> {
+fn parse_cmd(command: &format::Command) -> Result<Command, ParseError> {
     let parse_fn = match command.cmd.as_str() {
         "open" => Command::parse_open,
         "store" => Command::parse_store,
@@ -102,33 +102,12 @@ pub enum Command {
 }
 
 impl Command {
-    fn parse_open(c: &format::Command) -> Result<Command> {
+    fn parse_open(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::Open(c.target.clone()))
     }
 
-    fn parse_store_text(c: &format::Command) -> Result<Command> {
-        let mut targets = Vec::new();
-        for target in &c.targets {
-            let (target, tag) = match target.get(0..2) {
-                Some([target, tag]) => (target, tag),
-                _ => Err(ParseError::LocatorFormatError(
-                    "targets wrong format".to_owned(),
-                ))?,
-            };
-
-            let location = parse_location(&target)?;
-
-            let tag = tag
-                .splitn(2, ':')
-                .nth(1)
-                .ok_or(ParseError::LocatorFormatError(
-                    "type of selector is unknown".to_owned(),
-                ))?;
-            targets.push(Target {
-                location,
-                tag: Some(tag.to_owned()),
-            })
-        }
+    fn parse_store_text(c: &format::Command) -> Result<Command, ParseError> {
+        let targets = parse_targets(&c.targets)?;
         let var = c.value.clone();
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
@@ -140,7 +119,7 @@ impl Command {
         })
     }
 
-    fn parse_execute_script(c: &format::Command) -> Result<Command> {
+    fn parse_execute_script(c: &format::Command) -> Result<Command, ParseError> {
         let var = if c.value.is_empty() {
             None
         } else {
@@ -151,7 +130,7 @@ impl Command {
         Ok(Command::Execute { script, var })
     }
 
-    fn parse_wait_for_visible(c: &format::Command) -> Result<Command> {
+    fn parse_wait_for_visible(c: &format::Command) -> Result<Command, ParseError> {
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
         let timeout = cast_timeout(&c.value)?;
@@ -159,7 +138,7 @@ impl Command {
         Ok(Command::WaitForElementVisible { target, timeout })
     }
 
-    fn parse_wait_for_editable(c: &format::Command) -> Result<Command> {
+    fn parse_wait_for_editable(c: &format::Command) -> Result<Command, ParseError> {
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
         let timeout = cast_timeout(&c.value)?;
@@ -167,7 +146,7 @@ impl Command {
         Ok(Command::WaitForElementEditable { target, timeout })
     }
 
-    fn parse_wait_for_not_present(c: &format::Command) -> Result<Command> {
+    fn parse_wait_for_not_present(c: &format::Command) -> Result<Command, ParseError> {
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
         let timeout = cast_timeout(&c.value)?;
@@ -175,7 +154,7 @@ impl Command {
         Ok(Command::WaitForElementNotPresent { target, timeout })
     }
 
-    fn parse_select(c: &format::Command) -> Result<Command> {
+    fn parse_select(c: &format::Command) -> Result<Command, ParseError> {
         let locator = parse_select_locator(&c.value)?;
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
@@ -183,49 +162,49 @@ impl Command {
         Ok(Command::Select { target, locator })
     }
 
-    fn parse_echo(c: &format::Command) -> Result<Command> {
+    fn parse_echo(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::Echo(c.target.clone()))
     }
 
-    fn parse_while(c: &format::Command) -> Result<Command> {
+    fn parse_while(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::While(c.target.clone()))
     }
 
-    fn parse_if(c: &format::Command) -> Result<Command> {
+    fn parse_if(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::If(c.target.clone()))
     }
 
-    fn parse_else(_: &format::Command) -> Result<Command> {
+    fn parse_else(_: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::Else)
     }
 
-    fn parse_else_if(c: &format::Command) -> Result<Command> {
+    fn parse_else_if(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::ElseIf(c.target.clone()))
     }
 
-    fn parse_end(_: &format::Command) -> Result<Command> {
+    fn parse_end(_: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::End)
     }
 
-    fn parse_pause(c: &format::Command) -> Result<Command> {
+    fn parse_pause(c: &format::Command) -> Result<Command, ParseError> {
         let timeout = cast_timeout(&c.target)?;
         Ok(Command::Pause(timeout))
     }
 
-    fn parse_click(c: &format::Command) -> Result<Command> {
+    fn parse_click(c: &format::Command) -> Result<Command, ParseError> {
         let location = parse_location(&c.target)?;
         let target = Target::new(location);
         Ok(Command::Click(target))
     }
 
-    fn parse_store(c: &format::Command) -> Result<Command> {
+    fn parse_store(c: &format::Command) -> Result<Command, ParseError> {
         Ok(Command::Store {
             value: c.target.to_owned(),
             var: c.value.to_owned(),
         })
     }
 
-    fn parse_set_window_size(c: &format::Command) -> Result<Command> {
+    fn parse_set_window_size(c: &format::Command) -> Result<Command, ParseError> {
         let settings = c.target.split("x").map(|n| n.parse()).collect::<Vec<_>>();
         if settings.len() != 2 {
             Err(ParseError::TypeError(
@@ -278,7 +257,7 @@ pub enum Location {
 }
 
 impl Location {
-    fn new(tp: &str, path: &str) -> Result<Self> {
+    fn new(tp: &str, path: &str) -> Result<Self, ParseError> {
         let path = path.to_owned();
         match tp {
             "xpath" => Ok(Location::XPath(path)),
@@ -292,7 +271,7 @@ impl Location {
     }
 }
 
-fn parse_location(text: &str) -> Result<Location> {
+fn parse_location(text: &str) -> Result<Location, ParseError> {
     let mut target_location = text.splitn(2, '=');
     let location_type = target_location
         .next()
@@ -308,7 +287,7 @@ fn parse_location(text: &str) -> Result<Location> {
     Location::new(location_type, location)
 }
 
-fn parse_select_locator(text: &str) -> Result<SelectLocator> {
+fn parse_select_locator(text: &str) -> Result<SelectLocator, ParseError> {
     const ERROR_TEXT: &str = "unexpected type of selector";
 
     let mut locator = text.splitn(2, '=');
@@ -325,7 +304,37 @@ fn parse_select_locator(text: &str) -> Result<SelectLocator> {
     }
 }
 
-fn cast_timeout(s: &str) -> result::Result<Duration, ParseError> {
+fn parse_targets(targets: &[Vec<String>]) -> Result<Vec<Target>, ParseError> {
+    let mut out = Vec::new();
+    for target in targets {
+        if target.len() != 2 {
+            Err(ParseError::LocatorFormatError(
+                "targets wrong format".to_owned(),
+            ))?
+        }
+
+        let location = parse_location(&target[0])?;
+        let tag = parse_target_tag(&target[1])?.to_owned();
+
+        let target = Target {
+            location,
+            tag: Some(tag),
+        };
+        out.push(target);
+    }
+
+    Ok(out)
+}
+
+fn parse_target_tag<'a>(tag: &'a str) -> Result<&'a str, ParseError> {
+    tag.splitn(2, ':')
+        .nth(1)
+        .ok_or(ParseError::LocatorFormatError(
+            "type of selector is unknown".to_owned(),
+        ))
+}
+
+fn cast_timeout(s: &str) -> Result<Duration, ParseError> {
     // TODO: posibly there may be a variable not only a number
     // so we would need to create a enum like
     // enum Value<T> {
@@ -365,5 +374,82 @@ pub mod format {
         pub target: String,
         pub targets: Vec<Vec<String>>,
         pub value: String,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _parse() {
+        let file = side_file();
+        let reader = file.as_slice();
+        let tests = parse(reader);
+        assert!(tests.is_ok());
+        let tests = tests.unwrap();
+        assert_eq!(tests.len(), 1);
+        assert_eq!(tests[0].commands.len(), 3);
+        assert!(matches!(
+            tests[0].commands[0],
+            Command::Open(ref url) if url == "RELATIVE_URL/"
+        ));
+        assert!(matches!(
+            tests[0].commands[1],
+            Command::Pause(ref timeout) if *timeout == Duration::from_secs(5)
+        ));
+        assert!(matches!(
+            tests[0].commands[2],
+            Command::Execute {ref script, ref var} if script == "return \"hello world\"" && var == &Some("variable_name".to_string())
+        ));
+    }
+
+    fn side_file() -> Vec<u8> {
+        r#"{
+            "id": "bfc1bd56-39bd-4a0d-be2b-583ad75ac104",
+            "version": "2.0",
+            "name": "EXAMPLE_TEST",
+            "url": "https://BASE_URL.com/",
+            "tests": [{
+              "id": "5d61ce01-d373-4b14-a1a1-7474a4e192e5",
+              "name": "basic",
+              "commands": [{
+                "id": "5de247d8-432a-4c79-a44c-2e1fdc0c0ff9",
+                "comment": "",
+                "command": "open",
+                "target": "RELATIVE_URL/",
+                "targets": [],
+                "value": ""
+              }, {
+                "id": "3c00633c-4237-4c1b-b1e5-b8c2fc05e57d",
+                "comment": "",
+                "command": "pause",
+                "target": "5000",
+                "targets": [],
+                "value": ""
+              }, {
+                "id": "b59f9f80-aef4-44cc-a7f2-41d34d7ad482",
+                "comment": "",
+                "command": "executeScript",
+                "target": "return \"hello world\"",
+                "targets": [],
+                "value": "variable_name"
+              }]
+            }],
+            "suites": [{
+              "id": "925de5ce-03ae-4dcb-9146-c956ff3f090d",
+              "name": "Default Suite",
+              "persistSession": false,
+              "parallel": false,
+              "timeout": 300,
+              "tests": ["5d61ce01-d373-4b14-a1a1-7474a4e192e5"]
+            }],
+            "urls": ["https://bsscommerce.com/magento-2-one-step-checkout-extension.html"],
+            "plugins": []
+          }"#
+        .as_bytes()
+        .iter()
+        .cloned()
+        .collect()
     }
 }

6720b7013a3728d8fa2823d1f8da089b298f49a6
Maxim Zhiburt <zhiburt@gmail.com>
2020-09-07 14:13:21 +0300

    Make errors public
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ mod parser;
 mod runner;
 mod validation;
 
-pub use error::Result;
+pub use error::{Result, SideRunnerError, ParseError};
 pub use parser::{parse, Command, Test, File};
 pub use runner::Runner;

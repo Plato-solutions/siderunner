2897ee38880230b29cd01373f42b78e4e53b2e57
Maxim Zhiburt <zhiburt@gmail.com>
2021-06-11 19:35:16 +0300

    Include missed files
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ examples/wiki_fantoccini.rs
@@ -0,0 +1,29 @@
+/// The example requires a webdriver have been run
+
+fn main() {
+    panic!("This example requires 'fantoccini_backend' feature")
+}
+
+#[cfg(feature = "fantoccini_backend")]
+#[tokio::main]
+async fn main() {
+    use fantoccini::Client;
+    use pantheon::{parse, Runner};
+    
+    let client = Client::new("http://localhost:4444")
+        .await
+        .expect("can't connect to webdriver");
+    let wiki = std::fs::File::open("examples/wiki.side").unwrap();
+    let file = parse(wiki).expect("parsing can't be done...");
+    let mut runner = Runner::new(&client);
+    runner.run(&file.tests[0]).await.unwrap();
+
+    assert_eq!(
+        runner.data.get("slogan"),
+        Some(&serde_json::Value::String(
+            "The Free Encyclopedia".to_owned()
+        ))
+    );
+
+    runner.close().await.unwrap();
+}
---
+++ src/webdriver/thirtyfour.rs
@@ -0,0 +1,250 @@
+#![cfg(feature = "thirtyfour_backend")]
+
+use super::{Element, Locator, Webdriver};
+use serde_json::Value as Json;
+use std::time::Duration;
+use thirtyfour::{By, OptionRect, ScriptArgs, WebDriverCommands, components::select::SelectElement, error::WebDriverError};
+use url::Url;
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+pub struct Client<'a>(pub &'a thirtyfour::WebDriver);
+
+#[async_trait::async_trait]
+impl<'a> Webdriver for Client<'a> {
+    type Element = WebElement<'a>;
+    type Error = crate::error::RunnerErrorKind;
+
+    async fn goto(&mut self, url: &str) -> Result<(), Self::Error> {
+        self.0.get(url).await?;
+        Ok(())
+    }
+
+    async fn find(&mut self, locator: Locator) -> Result<Self::Element, Self::Error> {
+        let e = self.0.find_element((&locator).into()).await?;
+        Ok(WebElement(e, &self.0))
+    }
+
+    async fn find_all(&mut self, locator: Locator) -> Result<Vec<Self::Element>, Self::Error> {
+        let elements = self
+            .0
+            .find_elements((&locator).into())
+            .await?
+            .into_iter()
+            .map(move |e| WebElement(e, &self.0))
+            .collect();
+        Ok(elements)
+    }
+
+    async fn wait_for_visible(
+        &mut self,
+        locator: Locator,
+        timeout: Duration,
+    ) -> Result<Option<Duration>, Self::Error> {
+        let locator: By = (&locator).into();
+
+        let now = std::time::Instant::now();
+        loop {
+            match self.0.find_element(locator.clone()).await {
+                Ok(e) => {
+                    if e.is_displayed().await? {
+                        break Ok(None);
+                    }
+                }
+                Err(WebDriverError::NoSuchElement(..)) => (),
+                Err(err) => Err(err)?,
+            }
+
+            if now.elapsed() > timeout {
+                break Ok(Some(now.elapsed()));
+            }
+        }
+    }
+
+    async fn wait_for_not_present(
+        &mut self,
+        locator: Locator,
+        timeout: Duration,
+    ) -> Result<Option<Duration>, Self::Error> {
+        let locator: By = (&locator).into();
+
+        let now = std::time::Instant::now();
+        loop {
+            match self.0.find_element(locator.clone()).await {
+                Ok(..) => {}
+                Err(WebDriverError::NoSuchElement(..)) => break Ok(None),
+                Err(err) => Err(err)?,
+            }
+
+            if now.elapsed() > timeout {
+                break Ok(Some(now.elapsed()));
+            }
+        }
+        // std::thread::sleep_ms(4000);
+    }
+
+    async fn wait_for_present(
+        &mut self,
+        locator: Locator,
+        timeout: Duration,
+    ) -> Result<Option<Duration>, Self::Error> {
+        let locator: By = (&locator).into();
+
+        let now = std::time::Instant::now();
+        loop {
+            match self.0.find_element(locator.clone()).await {
+                Ok(..) => break Ok(None),
+                Err(WebDriverError::NoSuchElement(..)) => (), // TODO: sleep
+                Err(err) => Err(err)?,
+            }
+
+            if now.elapsed() > timeout {
+                break Ok(Some(now.elapsed()));
+            }
+        }
+    }
+
+    async fn wait_for_editable(
+        &mut self,
+        locator: Locator,
+        timeout: Duration,
+    ) -> Result<Option<Duration>, Self::Error> {
+        let now = std::time::Instant::now();
+        loop {
+            match self.0.find_element((&locator).into()).await {
+                Ok(element) => {
+                    let is_displayed = match element.get_attribute("style").await? {
+                        Some(style) if !style.contains("display: none;") => true,
+                        None => true,
+                        _ => false,
+                    };
+
+                    let is_enabled = match element.get_attribute("disabled").await? {
+                        Some(..) => false,
+                        _ => true,
+                    };
+
+                    if is_displayed && is_enabled {
+                        break Ok(None);
+                    }
+                }
+                Err(WebDriverError::NoSuchElement(..)) => {}
+                Err(err) => Err(err)?,
+            }
+
+            if now.elapsed() > timeout {
+                break Ok(Some(now.elapsed()));
+            }
+        }
+        // TODO: ...
+        // TODO: #issue https://github.com/jonhoo/fantoccini/issues/93
+    }
+
+    async fn current_url(&mut self) -> Result<Url, Self::Error> {
+        let url = self.0.current_url().await?;
+        Ok(Url::parse(&url).unwrap())
+    }
+
+    async fn set_window_size(&mut self, width: u32, height: u32) -> Result<(), Self::Error> {
+        self.0
+            .set_window_rect(OptionRect::new().with_size(width as i32, height as i32))
+            .await?;
+        Ok(())
+    }
+
+    async fn execute(&mut self, script: &str, a: Vec<Json>) -> Result<Json, Self::Error> {
+        let mut args = ScriptArgs::new();
+        for v in a {
+            args.push_value(v);
+        }
+
+        let ret = self.0.execute_script_with_args(script, &args).await?;
+        let json = ret.value().clone();
+
+        Ok(json)
+    }
+
+    async fn close(&mut self) -> Result<(), Self::Error> {
+        self.0.close().await?;
+        Ok(())
+    }
+}
+
+pub struct WebElement<'a>(thirtyfour::WebElement<'a>, &'a thirtyfour::WebDriver);
+
+#[async_trait::async_trait]
+impl<'a> Element for WebElement<'a> {
+    type Driver = Client<'a>;
+    type Error = crate::error::RunnerErrorKind;
+
+    async fn attr(&mut self, attribute: &str) -> Result<Option<String>, Self::Error> {
+        let attr = self.0.get_attribute(attribute).await?;
+        Ok(attr)
+    }
+
+    async fn prop(&mut self, prop: &str) -> Result<Option<String>, Self::Error> {
+        let prop = self.0.get_property(prop).await?;
+        Ok(prop)
+    }
+
+    async fn text(&mut self) -> Result<String, Self::Error> {
+        let text = self.0.text().await?;
+        Ok(text)
+    }
+
+    async fn html(&mut self, inner: bool) -> Result<String, Self::Error> {
+        let html = if inner {
+            self.0.inner_html().await?
+        } else {
+            self.0.outer_html().await?
+        };
+
+        Ok(html)
+    }
+
+    async fn find(&mut self, search: Locator) -> Result<Self, Self::Error>
+    where
+        Self: Sized,
+    {
+        let search: By = (&search).into();
+        let e = self.0.find_element(search).await?;
+
+        Ok(Self(e, self.1))
+    }
+
+    async fn click(mut self) -> Result<Self::Driver, Self::Error> {
+        self.0.click().await?;
+        Ok(Client(self.1))
+    }
+
+    async fn select_by_index(mut self, index: usize) -> Result<Self::Driver, Self::Error> {
+        SelectElement::new(&self.0)
+            .await?
+            .select_by_index(index as u32)
+            .await?;
+
+        Ok(Client(self.1))
+    }
+
+    async fn select_by_value(mut self, value: &str) -> Result<Self::Driver, Self::Error> {
+        SelectElement::new(&self.0)
+            .await?
+            .select_by_value(value)
+            .await?;
+
+        Ok(Client(self.1))
+    }
+}
+
+impl<'a> Into<By<'a>> for &'a Locator {
+    fn into(self) -> By<'a> {
+        match self {
+            Locator::LinkText(s) => By::LinkText(&s),
+            Locator::Css(s) => By::Css(&s),
+            Locator::Id(s) => By::Id(&s),
+            Locator::XPath(s) => By::XPath(&s),
+        }
+    }
+}

ba70cdd6fde1989ca55916e54394d032837708f5
Maxim Zhiburt <zhiburt@gmail.com>
2021-06-15 13:52:48 +0300

    Fix cargo fmt
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,21 @@
 //! ```
 //! use siderunner::{parse, Runner};
 //! use thirtyfour::{DesiredCapabilities, WebDriver};
-//! 
+//!
 //! let wiki = std::fs::File::open("examples/wiki.side").unwrap();
 //! let file = parse(wiki).expect("parsing can't be done...");
-//! 
+//!
 //! let client = WebDriver::new("http://localhost:4444", DesiredCapabilities::firefox())
 //!     .await
 //!     .expect("can't connect to webdriver");
 //! let mut runner = Runner::new(&client);
 //! runner.run(&file.tests[0]).await.unwrap();
-//! 
+//!
 //! assert_eq!(
 //!     runner.get_data().get("slogan"),
 //!     Some(&serde_json::json!("The Free Encyclopedia")),
 //! );
-//! 
+//!
 //! runner.close().await.unwrap();
 //! ```
 

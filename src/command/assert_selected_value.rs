4b6ec434003294d2c41cf67148f5669714a803cd
Maxim Zhiburt <zhiburt@gmail.com>
2021-07-03 23:14:03 +0300

    Add missed files
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ src/command/assert_selected_value.rs
@@ -0,0 +1,37 @@
+use super::Command;
+use crate::{
+    error::RunnerErrorKind,
+    webdriver::{Element, Locator, Webdriver},
+};
+
+pub struct AssertSelectedValue {
+    target: Locator,
+    text: String,
+}
+
+impl AssertSelectedValue {
+    pub fn new(target: Locator, text: String) -> Self {
+        Self { target, text }
+    }
+}
+
+#[async_trait::async_trait]
+impl<D: Webdriver> Command<D> for AssertSelectedValue {
+    async fn run(&self, runner: &mut crate::runner::Runner<D>) -> Result<(), RunnerErrorKind> {
+        let mut el = runner
+            .get_webdriver()
+            .find(self.target.clone())
+            .await?;
+
+        let value = el.prop("value").await?.unwrap_or_else(String::new);
+
+        if value == self.text {
+            Ok(())
+        } else {
+            Err(RunnerErrorKind::AssertFailed {
+                lhs: value,
+                rhs: self.text.clone(),
+            })
+        }
+    }
+}
